24d4c3745191c3f182aa75be454a03064de68a54
moe <moemmakki@gmail.com>
2024-12-19 21:10:58 -0500

    remove man page

--- src/config.mk
+++ src/config.mk
@@ -2,10 +2,8 @@
 
 VERSION = 1.0
 
-MB = matching-brackets/src
-
 PREFIX = /usr/local
-MANPREFIX = $(PREFIX)/share/man
+MB = matching-brackets/src
 
 KAKCFLAGS = -std=c99 -O3 -g -D_POSIX_C_SOURCE=200809L -Wall -Wextra -Werror \
 	-pedantic -Wshadow -Wdeclaration-after-statement -Wunused-macros \
